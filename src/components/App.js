c292bcd40f4cc3c22a3799680d9886bf74ce0fe2
zero3062 <esung2446@gmail.com>
2020-05-23 11:00:38 +0900

    기초기반2
    기초기반2
    components, client, pages, styles 폴더 생성

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import { Route } from 'react-router-dom';
+
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                Hello
+            </div>
+        );
+    }
+}
+
+export default App;

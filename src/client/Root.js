4dcba8921328e94e238d3ab7e38f193ada8ae37c
zero3062 <esung246@gmail.com>
2020-09-26 21:22:08 +0900

    work title edit and favicon edit

--- src/client/Root.js
+++ src/client/Root.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './../shared/App.js';
 
 const Root = () => (
-    <BrowserRouter>
+    <HashRouter>
         <App/>
-    </BrowserRouter>
+    </HashRouter>
 );
 
 export default Root;
